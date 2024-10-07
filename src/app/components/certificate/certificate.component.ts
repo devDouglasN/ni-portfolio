8ca59e0080900d42ea988c546e85cff6c6d4da61
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-10-07 15:33:18 -0300

    Some changes

--- src/app/components/certificate/certificate.component.ts
+++ src/app/components/certificate/certificate.component.ts
@@ -11,13 +11,18 @@ export class CertificateComponent {
     {
       title: 'Google Cybersecurity Certificate',
       imageUrl: 'https://images.credly.com/size/680x680/images/0bf0f2da-a699-4c82-82e2-56dcf1f2e1c7/image.png',
-      credlyUrl: 'https://www.credly.com/badges/d8b49429-31b2-418c-9569-2aa5bafea1dc/linked_in_profile'
+      credlyUrl: 'https://www.credly.com/badges/63b022a1-a343-48c5-af43-9e66a769dd2e/linked_in_profile'
     },
     {
       title: 'Introduction to Cybersecurity',
       imageUrl: 'https://images.credly.com/size/680x680/images/af8c6b4e-fc31-47c4-8dcb-eb7a2065dc5b/I2CS__1_.png',
-      credlyUrl: 'https://www.credly.com/badges/3214cd5f-dcfd-4716-a7cf-a07bd6a8e918/linked_in_profile'
+      credlyUrl: 'https://www.credly.com/badges/a1300f5f-7397-4c2a-b0ae-6844c57ddd48/linked_in_profile'
     },
+    {
+      title: 'Fundamentos em Cibersegurança',
+      imageUrl: 'https://certs.ibsec.com.br/wp-content/uploads/tutor-certificates/IJQo4srfLB-ba732cdf12a2e23a.jpg',
+      credlyUrl: 'https://certs.ibsec.com.br/?cert_hash=ba732cdf12a2e23a'
+    }
   ];
 
   selectedCertificate: any = null;
